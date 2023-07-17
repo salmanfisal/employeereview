4491ac239306651f48cc8241daa0a597d013760e
salmanfisal <78951958+salmanfisal@users.noreply.github.com>
2023-07-18 00:39:03 +0530

    query

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,7 @@ import React from "react";
 import video from "../styles/images/Work_in_Progress.mp4";
 
 function Home() {
-  return <div>
+  return <div className="working">
      <video className = "workinprogress" src={video} autoPlay loop muted />
      <h1 className = "workin">WORK IN PROGRESS..... </h1>
          </div>;
